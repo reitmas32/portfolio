0d0d68abc3a73a781faaf2df5e75fda0190f9333
reitmas32 <rafa.zamora.rals@gmail.com>
2023-04-23 05:25:25 -0600

    refactor: Extract Components

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,38 @@
 import 'package:flutter/material.dart';
-
-import 'package:portfolio/ui/widget/portfolioAppBar.dart';
+import 'package:portfolio/ui/portfolio.dart';
+import 'package:provider/provider.dart';
+import 'package:portfolio/ui/providers/theme.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo'),
-    );
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ThemeProvider themeChangeProvider = ThemeProvider();
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentAppTheme();
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  void getCurrentAppTheme() async {
+    themeChangeProvider.setTheme =
+        await themeChangeProvider.themePreference.getTheme();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // ignore: prefer_const_constructors
-      appBar: PortfolioAppBar(),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+    return ChangeNotifierProvider.value(
+      value: themeChangeProvider,
+      child: const Portfolio(),
     );
   }
 }
