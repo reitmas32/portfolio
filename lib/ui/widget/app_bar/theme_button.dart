210d23d782418c6dae4338cf6aba31aaa054c783
reitmas32 <rafa.zamora.rals@gmail.com>
2023-04-23 06:05:07 -0600

    feat: Create ThemeButton

---
+++ lib/ui/widget/app_bar/theme_button.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/ui/providers/theme_provider.dart';
+import 'package:portfolio/ui/theme_preference.dart';
+import 'package:provider/provider.dart';
+
+class ThemeButton extends StatelessWidget {
+  const ThemeButton({
+    super.key,
+    required this.nameTheme,
+  });
+
+  final String nameTheme;
+
+  @override
+  Widget build(BuildContext context) {
+    final currentTheme = Provider.of<ThemeProvider>(context);
+    Function selectColors = () {
+      return Colors.grey;
+    };
+    void Function() onTap = () {};
+    IconData icon = Icons.wb_sunny;
+    
+    if (nameTheme == ThemePreference.LIGHT) {
+      
+      selectColors = () {
+        return currentTheme.isDarkTheme() ? Colors.grey : Colors.orange;
+      };
+      
+      onTap = () {
+        if (currentTheme.isDarkTheme()) {
+          currentTheme.setTheme = ThemePreference.LIGHT;
+        }
+      };
+      
+      icon = Icons.wb_sunny;
+    } else {
+      
+      selectColors = () {
+        return currentTheme.isDarkTheme() ? Colors.orange : Colors.grey;
+      };
+      onTap = () {
+        if (!currentTheme.isDarkTheme()) {
+          currentTheme.setTheme = ThemePreference.DARK;
+        }
+      };
+      
+      icon = Icons.brightness_2;
+    }
+
+    return InkWell(
+      borderRadius: BorderRadius.circular(25.0),
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Icon(icon, color: selectColors()),
+      ),
+    );
+  }
+}
