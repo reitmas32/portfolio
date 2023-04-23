4b3636eff73a34b661be73d789b4713c8facf1d9
reitmas32 <rafa.zamora.rals@gmail.com>
2023-04-23 06:10:27 -0600

    feat: Add Code to Counter, Hello World

--- lib/ui/pages/my_home_page.dart
+++ lib/ui/pages/my_home_page.dart
@@ -6,16 +6,40 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
+  // ignore: library_private_types_in_public_api
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+    int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: PortfolioAppBar(),
+    return Scaffold(
+      appBar: const PortfolioAppBar(),
       body: Center(
-        child: Text('Hello')
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
       ),
     );
   }
