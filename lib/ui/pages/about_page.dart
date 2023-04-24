d12f4afb19053c619d9cb0f2a7cfa307b5243be4
reitmas32 <rafa.zamora.rals@gmail.com>
2023-04-23 18:14:43 -0600

    feat: Create AboutPage

---
+++ lib/ui/pages/about_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/ui/widget/app_bar/portfolioAppBar.dart';
+
+class AboutPage extends StatefulWidget {
+  const AboutPage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _AboutPageState createState() => _AboutPageState();
+}
+
+class _AboutPageState extends State<AboutPage> {
+    int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const PortfolioAppBar(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'AboutPage',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
