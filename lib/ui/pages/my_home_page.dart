94e6dceee129e48ab9b6c0a45f1377f867063090
reitmas32 <rafa.zamora.rals@gmail.com>
2023-04-27 10:30:17 -0600

    feat: Create Home Banner

--- lib/ui/pages/my_home_page.dart
+++ lib/ui/pages/my_home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/ui/widget/app_bar/button_app_bar.dart';
 import 'package:portfolio/ui/widget/app_bar/portfolioAppBar.dart';
+import 'package:portfolio/ui/widget/home_banner/home_banner.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -11,30 +13,32 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-    int _counter = 0;
+  int _counter = 0;
 
   void _incrementCounter() {
     setState(() {
       _counter++;
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const PortfolioAppBar(),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: ListView(
+        children: [
+          const HomeBanner(),
+          const SizedBox(
+            height: 10,
+          ),
+          Image.network(
+              'https://raw.githubusercontent.com/reitmas32/portfolio/master/public/assets/home-banner.png'),
+          const SizedBox(
+            height: 10,
+          ),
+          Image.network(
+              'https://raw.githubusercontent.com/reitmas32/portfolio/master/public/assets/home-banner.png')
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
