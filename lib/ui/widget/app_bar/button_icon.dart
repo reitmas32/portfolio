d99e704065be12e870d6a9cb6e8cc7b41cc0a8c2
reitmas32 <rafa.zamora.rals@gmail.com>
2023-04-27 09:33:08 -0600

    feat: Create ButtonIcon Widget

---
+++ lib/ui/widget/app_bar/button_icon.dart
@@ -0,0 +1,44 @@
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class ButtonIcon extends StatelessWidget {
+  const ButtonIcon({
+    super.key,
+    required this.url,
+    this.external = false,
+    required this.iconData,
+  });
+
+  final String url;
+  final bool external;
+  final IconData iconData;
+
+  Future<void> _externalLaunchUrl() async {
+    if (!await launchUrl(Uri.parse(url))) {
+      throw Exception('Could not launch $url');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      borderRadius: BorderRadius.circular(10.0),
+      onTap: external ? _externalLaunchUrl : (){
+        context.go(url);
+      },
+      hoverColor: Theme.of(context).colorScheme.secondary,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(25.0),
+          child: Icon(
+            iconData,
+            size: 35.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
