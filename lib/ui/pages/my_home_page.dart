da6b59e3a824a8b6727286100fa9c766f2768b44
reitmas32 <rafa.zamora.rals@gmail.com>
2023-04-28 07:02:47 -0600

    feat: Remove Widget Unused

--- lib/ui/pages/my_home_page.dart
+++ lib/ui/pages/my_home_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:portfolio/ui/widget/app_bar/button_app_bar.dart';
 import 'package:portfolio/ui/widget/app_bar/portfolioAppBar.dart';
 import 'package:portfolio/ui/widget/fotter/fotter.dart';
-import 'package:portfolio/ui/widget/home_banner/home_banner.dart';
 import 'package:portfolio/ui/widget/home_body/home_body.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -15,32 +13,15 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const PortfolioAppBar(),
       body: ListView(
         children: const [
-          HomeBanner(),
-          SizedBox(
-            height: 400,
-          ),
           HomeBody(),
           Fotter(),
         ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
