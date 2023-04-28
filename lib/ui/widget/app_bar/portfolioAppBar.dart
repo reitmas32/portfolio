1524258f35990552039f377b40387678c02ae80e
reitmas32 <rafa.zamora.rals@gmail.com>
2023-04-28 07:03:28 -0600

    feat: remove files unused

--- lib/ui/widget/app_bar/portfolioAppBar.dart
+++ lib/ui/widget/app_bar/portfolioAppBar.dart
@@ -1,12 +1,10 @@
-// ignore: file_names
+// ignore_for_file: file_names
 import 'package:flutter/material.dart';
 import 'package:portfolio/ui/theme_preference.dart';
 import 'package:portfolio/ui/widget/app_bar/button_app_bar.dart';
 import 'package:portfolio/ui/widget/app_bar/button_icon.dart';
 import 'package:portfolio/ui/widget/app_bar/button_image.dart';
 import 'package:portfolio/ui/widget/app_bar/theme_button.dart';
-import 'package:provider/provider.dart';
-import 'package:portfolio/ui/providers/theme_provider.dart';
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 
 class PortfolioAppBar extends StatelessWidget implements PreferredSizeWidget {
@@ -15,6 +13,7 @@ class PortfolioAppBar extends StatelessWidget implements PreferredSizeWidget {
   });
 
   List<Widget> getActions(double width) {
+    // ignore: no_leading_underscores_for_local_identifiers
     List<Widget> _actions = [];
 
     if (width > 750) {
