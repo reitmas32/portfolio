91476bfb28567b407f1a699a48a63578e12b83e1
reitmas32 <rafa.zamora.rals@gmail.com>
2023-04-28 07:05:37 -0600

    feat: extract Widgets

--- lib/ui/widget/home_body/home_body.dart
+++ lib/ui/widget/home_body/home_body.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/ui/widget/home_banner/home_banner.dart';
+import 'package:portfolio/ui/widget/home_body/about_info.dart';
+import 'package:portfolio/ui/widget/home_body/selected_work.dart';
 
 class HomeBody extends StatelessWidget {
   const HomeBody({
@@ -7,105 +10,36 @@ class HomeBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(children: [
-      Expanded(flex: 1, child: Container(),),
-      Expanded(
-        flex: 3,
-        child: Container(
-          width: MediaQuery.of(context).size.width / 2,
-          //height: 300,
-          child: Padding(
-            padding: const EdgeInsets.all(50.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Selected Work',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: MediaQuery.of(context).size.width / 35,
-                  ),
-                  textAlign: TextAlign.start,
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  'Hola!',
-                  style: TextStyle(
-                    fontWeight: FontWeight.normal,
-                    fontSize: MediaQuery.of(context).size.width / 50,
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width / 5,
-                  child: Text(
-                    'Soy Rafa, ingeniero de computación entusiasta con 3 años de experiencia en la gestión de proyectos, lenguaje scripting, POO y Servicios de Escritorio Remoto.',
-                    overflow: TextOverflow.clip,
-                    maxLines: 6,
-                    style: TextStyle(
-                      fontWeight: FontWeight.normal,
-                      fontSize: MediaQuery.of(context).size.width / 80,
-                    ),
-                    textAlign: TextAlign.justify,
-                  ),
-                ),
-              ],
+    return Column(
+      children: [
+        const HomeBanner(),
+        const SizedBox(
+          height: 400,
+        ),
+        Row(children: [
+          Expanded(
+            flex: 1,
+            child: Container(),
+          ),
+          Expanded(
+            flex: 3,
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width / 2,
+              //height: 300,
+              child: const SelectedWork(),
             ),
           ),
-        ),
-      ),
-      Expanded(
-        flex: 2,
-        child: Container(
-          width: MediaQuery.of(context).size.width / 2,
-          //height: 300,
-          child: Padding(
-            padding: const EdgeInsets.all(50.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'About me',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: MediaQuery.of(context).size.width / 35,
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  'Hola!',
-                  style: TextStyle(
-                    fontWeight: FontWeight.normal,
-                    fontSize: MediaQuery.of(context).size.width / 50,
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width / 5,
-                  child: Text(
-                    'Soy Rafa, ingeniero de computación entusiasta con 3 años de experiencia en la gestión de proyectos, lenguaje scripting, POO y Servicios de Escritorio Remoto.',
-                    overflow: TextOverflow.clip,
-                    maxLines: 6,
-                    style: TextStyle(
-                      fontWeight: FontWeight.normal,
-                      fontSize: MediaQuery.of(context).size.width / 80,
-                    ),
-                    textAlign: TextAlign.justify,
-                  ),
-                ),
-              ],
+          Expanded(
+            flex: 2,
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width / 2,
+              //height: 300,
+              child: const AboutInfo(),
             ),
           ),
-        ),
-      ),
-    ]);
+        ]),
+      ],
+    );
   }
 }
+
