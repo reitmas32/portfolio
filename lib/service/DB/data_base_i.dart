9063282b22e99d82dcfe388d6e568094261fd84e
reitmas32 <rafa.zamora.rals@gmail.com>
2023-04-28 19:34:27 -0600

    feat: Create Interface by DataBase

---
+++ lib/service/DB/data_base_i.dart
@@ -0,0 +1,11 @@
+import 'package:portfolio/domain/models/project.dart';
+
+class DataBase{
+  List<Project> getProjects(){
+    return throw Exception('No implemented getProjects');
+  }
+
+  Project getLastProject(){
+    return throw Exception('No implemented getProjects');
+  }
+}
