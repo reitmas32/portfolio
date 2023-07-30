912227873730dbda48b74a65343b799df09b58fb
reitmas32 <rafa.zamora.rals@gmail.com>
2023-07-30 14:34:09 -0600

    refactor: remove load files

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,8 @@ import 'package:provider/provider.dart';
 import 'package:portfolio/ui/providers/theme_provider.dart';
 import 'package:url_strategy/url_strategy.dart';
 
-void main() {
+void main() async {
   setPathUrlStrategy();
-  dataBaseConnection.loadFiles();
   runApp(const MyApp());
 }
 
