91c5c440673169fe28a7b40a59f23133d1a5a452
reitmas32 <rafa.zamora.rals@gmail.com>
2023-07-30 14:38:34 -0600

    feat: create PortfolioSliverAppBar

--- lib/ui/widget/app_bar/portfolioAppBar.dart
+++ lib/ui/widget/app_bar/portfolioAppBar.dart
@@ -89,3 +89,84 @@ class PortfolioAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Size get preferredSize => const Size.fromHeight(100.0);
 }
+
+class PortfolioSliverAppBar extends StatelessWidget {
+  const PortfolioSliverAppBar({
+    super.key,
+  });
+
+  List<Widget> getActions(double width) {
+    // ignore: no_leading_underscores_for_local_identifiers
+    List<Widget> _actions = [];
+
+    if (width > 750) {
+      _actions.add(
+        const ButtonIcon(
+          url: 'https://twitter.com/rafazram',
+          external: true,
+          iconData: EvaIcons.linkedin,
+        ),
+      );
+      _actions.add(
+        const ButtonIcon(
+          url: 'https://github.com/reitmas32',
+          external: true,
+          iconData: EvaIcons.github,
+        ),
+      );
+      _actions.add(
+        const ButtonIcon(
+          url: 'https://twitter.com/rafazram',
+          external: true,
+          iconData: EvaIcons.twitter,
+        ),
+      );
+    }
+    _actions.add(const SizedBox(
+      width: 40.0,
+    ));
+    _actions.add(
+      const ThemeButton(
+        nameTheme: ThemePreference.LIGHT,
+      ),
+    );
+    _actions.add(
+      const ThemeButton(
+        nameTheme: ThemePreference.DARK,
+      ),
+    );
+    _actions.add(
+      const SizedBox(
+        width: 40.0,
+      ),
+    );
+
+    return _actions;
+  }
+
+  @override
+  SliverAppBar build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      actions: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: getActions(MediaQuery.of(context).size.width),
+        ),
+      ],
+      title: const Row(
+        children: [
+          ButtonImage(
+            url: '/',
+            imageUrl:
+                'https://raw.githubusercontent.com/reitmas32/portfolio/master/public/assets/logo.png',
+          ),
+          ButtonAppBar(lable: 'Projects'),
+          //ButtonAppBar(lable: 'About'),
+        ],
+      ),
+      floating: true,
+      snap: true,
+    );
+  }
+}
