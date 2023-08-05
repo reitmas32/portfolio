800cd3d7c1f9ece0ae2f3b9665a4e8c97523e7f5
reitmas32 <rafa.zamora.rals@gmail.com>
2023-08-05 14:46:47 -0600

    feat: Change padding

--- lib/ui/widget/home_body/selected_work.dart
+++ lib/ui/widget/home_body/selected_work.dart
@@ -14,7 +14,7 @@ class SelectedWork extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Padding(
-      padding: const EdgeInsets.all(50.0),
+      padding: size.width > 1000 ? EdgeInsets.all(50.0) : EdgeInsets.all(0.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
