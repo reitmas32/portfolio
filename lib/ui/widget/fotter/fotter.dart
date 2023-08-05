68038d87865416ae30a8cb8cd58b5d2d65f5dc35
reitmas32 <rafa.zamora.rals@gmail.com>
2023-08-05 14:44:36 -0600

    feat: configure to theme selected

--- lib/ui/widget/fotter/fotter.dart
+++ lib/ui/widget/fotter/fotter.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/ui/providers/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 class Fotter extends StatelessWidget {
   const Fotter({
@@ -7,8 +9,12 @@ class Fotter extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height / 3,
+    final currentTheme = Provider.of<ThemeProvider>(context);
+    return Container(
+      height: MediaQuery.of(context).size.height / 4,
+      color: currentTheme.isDarkTheme()
+          ? const Color.fromARGB(255, 94, 93, 93)
+          : Color.fromARGB(255, 196, 194, 194),
       child: const Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
