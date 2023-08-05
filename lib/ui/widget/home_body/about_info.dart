1589ed1463c2c9e63987d682eb52771760b5f46e
reitmas32 <rafa.zamora.rals@gmail.com>
2023-08-05 14:45:50 -0600

    feat: Change pading

--- lib/ui/widget/home_body/about_info.dart
+++ lib/ui/widget/home_body/about_info.dart
@@ -9,7 +9,7 @@ class AboutInfo extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Padding(
-      padding: const EdgeInsets.all(50.0),
+      padding: size.width > 1000 ? EdgeInsets.all(50.0) : EdgeInsets.all(0.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -33,12 +33,12 @@ class AboutInfo extends StatelessWidget {
           const SizedBox(
             height: 20,
           ),
-          const SelectableText(
-            'Soy un apasionado desarrollador de software y entusiasta de la tecnología con 2 años de experiencia en projectos OpenSource. Mi experiencia abarca el uso de diversas herramientas, entre ellas: Python, Go, C++, Flutter, Docker, GitHub, PostgreSQL y Linux. Con python he construidos APIs, cron jobs y herramientas de línea de comandos.En paralelotambien he utilizado Go para el desarrollo de APIs.Para el control de versiones y el despliegue automatizado, utilizo GitHub, Docker y el sistema PaaS Render.com, Soy una persona que le encatan siempre estar aprendiendo cosas nuevas y fielmente creyente que cosas realmente significativas solo se logran en equipo, intercambiando ideas y aprendiendo de los demas. Mi objetivo es continuar creciendo y contribuyendo en el campo del desarrollo de software.',
+          SelectableText(
+            'Soy un apasionado desarrollador de software y entusiasta de la tecnología con 2 años de experiencia en projectos OpenSource. Mi experiencia abarca el uso de diversas herramientas, entre ellas: Python, Go, C++, Flutter, Docker, GitHub, PostgreSQL y Linux. Con python he construidos APIs, cron jobs y herramientas de línea de comandos. En paralelotambien he utilizado Go para el desarrollo de APIs. Para el control de versiones y el despliegue automatizado, utilizo GitHub, Docker y el sistema PaaS Render.com, Soy una persona que le encatan siempre estar aprendiendo cosas nuevas y fielmente creyente que cosas realmente significativas solo se logran en equipo, intercambiando ideas y aprendiendo de los demas. Mi objetivo es continuar creciendo y contribuyendo en el campo del desarrollo de software.',
             maxLines: 20,
             style: TextStyle(
               fontWeight: FontWeight.normal,
-              fontSize: 20.0,
+              fontSize: size.width / 50 > 20 ? 20 : 15,
               height: 1.7,
             ),
             textAlign: TextAlign.justify,
