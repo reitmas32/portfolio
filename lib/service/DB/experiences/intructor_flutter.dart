114a75ae23f590a3e866bfb4bc42aadd712e8e99
reitmas32 <rafa.zamora.rals@gmail.com>
2023-09-11 01:25:45 -0600

    feat: update description of InstructorFlutter

--- lib/service/DB/experiences/intructor_flutter.dart
+++ lib/service/DB/experiences/intructor_flutter.dart
@@ -5,6 +5,11 @@ Experience InstructorFlutter = Experience(
   end: 'ABRIL 2023',
   title: 'Instructor de Curso Flutter',
   company: 'FI UNAM',
-  description:
-      'Dirigí un curso básico de Flutter en la UNAM, introduciendo a estudiantes a la creación de aplicaciones con este versátil framework de Google. A lo largo del curso, realizamos tres proyectos clave: Calculadora Móvil: Una aplicación inicial para familiarizar a los alumnos con Flutter y Dart, enfocándonos en una interfaz intuitiva. Web Personal: Demostramos la capacidad de Flutter para desarrollos web, guiando a los estudiantes en la creación de un sitio personal. Clone GUI de ChatGPT: Nuestro proyecto más ambicioso, desarrollando una interfaz gráfica conectada a la API de ChatGPT, enseñando integración de APIs y diseño de interfaces para chatbots. Esta experiencia en la UNAM no solo impartió habilidades técnicas, sino que fomentó un intercambio enriquecedor con estudiantes apasionados en un ambiente académico de vanguardia.',
+  description: """
+Dirigí un curso básico de Flutter en la UNAM, introduciendo a estudiantes a la creación de aplicaciones con este versátil framework de Google. A lo largo del curso, realizamos tres proyectos clave: 
+
+- Calculadora Móvil 
+- Web Personal
+- Clone GUI de ChatGPT
+""",
 );
